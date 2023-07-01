1b37cb065ccc016e3544aac1b18d0e84ac68685e
Bao-thik-coding <113531784+Bao-thik-coding@users.noreply.github.com>
2023-07-01 20:28:29 +0700

    Add project files.

---
+++ CGO_buoi01/Program.cs
@@ -0,0 +1,21 @@
+﻿//im not good im not better im the best
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**CGO_buoi01
+ class: xây dựng đối tượng quản lý
+references 
+Main: hàm kích hoạt/ chạy đầu*/
+namespace CGO_buoi01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime Time = DateTime.Now;
+            System.Console.WriteLine("Bây giờ là  " + Time);
+        }
+    }
+}
